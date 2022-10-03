a8d2c54375e2d7fb60be5a04d4642c86807fbafb
kacrisa <kacrisa@outlook.com.ar>
2022-10-03 14:25:36 -0300

    Empezando con el Auth

--- Desafio Subir de Modulo - M3/Maqueta-Angular/src/app/Componentes/nav-bar/nav-bar.component.ts
+++ Desafio Subir de Modulo - M3/Maqueta-Angular/src/app/Componentes/nav-bar/nav-bar.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/servicios/auth/auth.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
-  
+
 })
 export class NavBarComponent implements OnInit {
   isAdmin:boolean
 
-  constructor() { 
+
+  constructor( ) {
     this.isAdmin =  false
   }
 
